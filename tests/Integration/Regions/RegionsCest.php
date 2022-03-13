7cb5a8e55e12f6b68572af7ae20ad9f101c53b37
kaioken <max@mctekk.com>
2022-03-13 08:41:39 -0400

    feat : variants attributes

--- tests/Integration/Regions/RegionsCest.php
+++ tests/Integration/Regions/RegionsCest.php
@@ -4,38 +4,15 @@ declare(strict_types=1);
 
 namespace Kanvas\Inventory\Tests\Integration\Categories;
 
-use Canvas\Models\Currencies;
 use IntegrationTester;
-use Kanvas\Inventory\Enums\State;
 use Kanvas\Inventory\Regions\Models\Regions;
-use Kanvas\Inventory\Regions\Region;
 use Kanvas\Inventory\Tests\Support\Models\Users;
+use Kanvas\Inventory\Tests\Support\Traits\CanCreateRegion;
 
 class RegionsCest
 {
-    /**
-     * Create a Regions.
-     *
-     * @return Regions
-     */
-    protected function createRegion(IntegrationTester $I) : Regions
-    {
-        $user = new Users();
-        $region = Regions::findFirst();
-        $currency = Currencies::findFirst();
+    use CanCreateRegion;
 
-        $regions = Region::create(
-            $user,
-            $I->faker()->name(),
-            $currency,
-            [
-                'is_published' => State::PUBLISHED,
-                'is_default' => State::DEFAULT,
-            ]
-        );
-
-        return $regions;
-    }
 
     public function tesCreate(IntegrationTester $I) : void
     {
